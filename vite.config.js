75058a86eb4dc2c4045c45c3ebb449e54fe6524f
Mohamed Rafat <121605610+MohamedRafatabotaleb@users.noreply.github.com>
2024-11-18 12:56:14 +0200

    conf

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-    },
-  },
   plugins: [react()],
 });
