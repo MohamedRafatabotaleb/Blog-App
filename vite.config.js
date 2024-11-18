4a368b36508a739a0293c058812e24a5d07bc71b
Mohamed Rafat <121605610+MohamedRafatabotaleb@users.noreply.github.com>
2024-11-18 14:06:33 +0200

    va

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path-browserify"; // استخدام path-browserify
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      // استبدال path بـ path-browserify
-      path: "path-browserify",
-    },
-  },
   plugins: [react()],
 });
